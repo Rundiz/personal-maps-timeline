3beb8c0e6e813ef650e5ce4f72e0e91332c47df0
ve3 <kawewong@gmail.com>
2025-05-14 00:34:24 +0700

    Fix retrieve incorrect start & end years on **summary-visited.php**.
    From previous, it was retrieved data based on latitude and longitude but sometime this can changed but however the Google Place ID won't change. Fix by update the code to retrieve data again based on place ID found on lat&lng.

--- HTTP/summary-visited.php
+++ HTTP/summary-visited.php
@@ -38,13 +38,15 @@ if (empty($errorMessage)) {
     // if there is no errors.
     $output['visitedPlace'] = [];
     $googleExportedLatLngFormat = $latitude . '°, ' . $longitude . '°';
+    $minMaxYear = new \stdClass();
 
-    // retrieve first and last year visited. ====================
+    // retrieve first and last year visited base on latitude and longitude only. --------------
     $sql = 'SELECT YEAR(MIN(`startTime`)) AS `startYear`, YEAR(MAX(`endTime`)) AS `endYear`
         , `v`.`visit_id`, `v`.`topCandidate_placeId`, `v`.`topCandidate_placeLocation_latLng`
     FROM `visit` AS `v`
     INNER JOIN `semanticsegments` AS `s` ON `v`.`segment_id` = `s`.`id`
-    WHERE `topCandidate_placeLocation_latLng` = :latlng';
+    WHERE `topCandidate_placeLocation_latLng` = :latlng
+    ORDER BY `startYear` ASC, `endYear` ASC';
     $Sth = $dbh->prepare($sql);
     unset($sql);
     $Sth->bindValue(':latlng', $googleExportedLatLngFormat);
@@ -53,9 +55,57 @@ if (empty($errorMessage)) {
     $Sth->closeCursor();
     unset($Sth);
     if ($row) {
-        $output['visitedPlace']['minMaxYears'] = $row;
+        $output['visitedPlace']['summaryDataBaseOnLatLng'] = $row;
+        $minMaxYear = $row;
+        $placeId = $row->topCandidate_placeId;
     }
     unset($row);
+
+    // retrieve first and last year visited base on Google Place ID. ------------------------------
+    if (isset($placeId)) {
+        $sql = 'SELECT * 
+        FROM (
+            SELECT DISTINCT YEAR(`startTime`) AS `yearVisited` 
+                , YEAR(`startTime`) AS `startYear`, YEAR(`endTime`) AS `endYear`
+                , `visit_id`, `topCandidate_placeId`, `topCandidate_placeLocation_latLng`
+            FROM `visit`
+            INNER JOIN `semanticsegments` AS `s` ON `segment_id` = `s`.`id`
+            WHERE `topCandidate_placeId` = :placeId
+            UNION
+            SELECT DISTINCT YEAR(`endTime`) AS `yearVisited` 
+                , YEAR(`startTime`) AS `startYear`, YEAR(`endTime`) AS `endYear`
+                , `visit_id`, `topCandidate_placeId`, `topCandidate_placeLocation_latLng`
+            FROM `visit`
+            INNER JOIN `semanticsegments` AS `s` ON `segment_id` = `s`.`id`
+        ) AS `yearVisitedUnion`
+        WHERE `topCandidate_placeId` = :placeId
+        GROUP BY `yearVisited`
+        ORDER BY `yearVisitedUnion`.`yearVisited` ASC';
+        $Sth = $dbh->prepare($sql);
+        unset($sql);
+        $Sth->bindValue(':placeId', $placeId);
+        $Sth->execute();
+        $result = $Sth->fetchAll();
+        $Sth->closeCursor();
+        unset($Sth);
+        if (isset($result)) {
+            $output['visitedPlace']['summaryDataBaseOnPlaceId'] = $result;
+            foreach ($result as $row) {
+                if ($row->yearVisited < $minMaxYear->startYear) {
+                    $minMaxYear->startYear = $row->yearVisited;
+                }
+                if ($row->yearVisited > $minMaxYear->endYear) {
+                    $minMaxYear->endYear = $row->yearVisited;
+                }
+            }// endforeach;
+            unset($row);
+        }// endif; there is fetched result.
+        unset($placeId);
+        unset($result);
+    }// endif; there is place ID value.
+
+    $output['visitedPlace']['minMaxYears'] = $minMaxYear;
+    unset($minMaxYear);
 
     unset($googleExportedLatLngFormat);
 }// endif; there is no errors.
